2172e2ecc76287d61a651b1129ad45c5e36999c4
almtronics <morneauandrelouis@gmail.com>
2026-02-27 17:29:23 -0500

    Add custom app icon

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ Application entry point for the THz-TDS Toolkit GUI.
 - Start the Tk event loop
 """
 import tkinter as tk
+import os
+import sys
 
 from core.data_manager import DataManager
 from ui.app_window import THzToolkitApp
@@ -16,9 +18,21 @@ from pages.time_page import TimePage
 from pages.frequency_page import FrequencyPage
 from pages.phase_page import PhasePage
 
+def resource_path(relative_path):
+    """ Get absolute path to resource """
+    try: # PyInstaller temp folder at runtime
+        base_path = sys._MEIPASS
+    except Exception: # Not runtime, use standard path
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 def main() -> None:
     """Launch the application."""
     root = tk.Tk()
+    icon_path = resource_path(os.path.join("img", "TTT.ico"))
+    root.iconbitmap(icon_path)
+
     data_mgr = DataManager()
     app = THzToolkitApp(root, data_mgr)
     # Register Pages
